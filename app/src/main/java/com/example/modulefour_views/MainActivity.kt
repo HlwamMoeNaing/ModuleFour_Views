59bd69ae5a921222d7efcafab355e07cd701f464
hlwam <hlwammoe.naing@kbzbank.com>
2022-12-16 00:17:00 +0630

    second

--- app/src/main/java/com/example/modulefour_views/MainActivity.kt
+++ app/src/main/java/com/example/modulefour_views/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.modulefour_views
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.modulefour_views.MyTests.ImageViewActivity
+import com.example.modulefour_views.assignments.ImageEdTextAndBtnAssignmentActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-btnGoImageView.setOnClickListener {
-    startActivity(Intent(this,ImageViewActivity::class.java))
-}
+        btnGoImageView.setOnClickListener {
+            startActivity(Intent(this, ImageViewActivity::class.java))
+        }
+
+
+        btnGoAssignmentOne.setOnClickListener {
+            startActivity(Intent(this,ImageEdTextAndBtnAssignmentActivity::class.java))
+        }
     }
 }
